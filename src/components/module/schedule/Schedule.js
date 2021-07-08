582881beef71adc1515646e26f967d9bc6ac4868
praveenfrontend <praveenfrontend@gmail.com>
2021-07-09 01:30:28 +0530

    fixed schedule issues

--- src/components/module/schedule/Schedule.js
+++ src/components/module/schedule/Schedule.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Modal } from "react-bootstrap";
 import Axios from "axios";
 import { Calendar, momentLocalizer, Views } from "react-big-calendar";
 import moment from "moment";
+
 // import localizer from "react-big-calendar/lib/localizers/globalize";
 import Page from "../../common/Page";
 
@@ -22,14 +22,6 @@ function Schedule() {
   const [editModal, setEditModal] = useState(false);
   const [eventObj, setEventObj] = useState({});
 
-  // const [events, setEvents] = useState([
-  //   {
-  //     id: 0,
-  //     title: "Zumba",
-  //     start: new Date(2021, 3, 7),
-  //     end: new Date(2021, 3, 7)
-  //   }
-  // ]);
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
@@ -45,6 +37,7 @@ function Schedule() {
           response.data.map(program => {
             const event = {
               id: program.id,
+              programName: program.ProgramName,
               userId: program.UserID,
               title: program.Title,
               start: program.StartDate,
@@ -57,10 +50,6 @@ function Schedule() {
             eventArray.push(event);
           });
           setEvents([...eventArray]);
-          // swal(`Schedule Created.`, "Program will be added to the calendar.", "success").then(res => {
-          // setLoading(true);
-          // window.location.reload();
-          // });
         }
       } catch (e) {
         swal("Something went wrong", e.response.error, "error");
@@ -69,31 +58,6 @@ function Schedule() {
     }
     getSchedules();
   }, []);
-
-  const handleSelect = ({ start, end }) => {
-    const title = window.prompt("New Event name");
-    if (title) {
-      // this.setState({
-      //   events: [
-      //     ...this.state.events,
-      //     {
-      //       start,
-      //       end,
-      //       title
-      //     }
-      //   ]
-      // });
-
-      setEvents([
-        ...events,
-        {
-          start,
-          end,
-          title
-        }
-      ]);
-    }
-  };
 
   const scheduleModalForm = () => {
     setScheduleModal(true);
@@ -111,8 +75,15 @@ function Schedule() {
   return (
     <>
       <Page title="Schedule" schedule={true} clickHandler={scheduleModalForm}>
-        <Calendar selectable localizer={localizer} defaultDate={new Date()} /* defaultView="month" */ defaultView={Views.MONTH} events={events} style={{ height: "100vh" }} onSelectEvent={event => getScheduleById(event)} /* onSelectSlot={handleSelect} */ />
-        {/* <Calendar selectable localizer={localizer} events={events} defaultView={Views.MONTH} scrollToTime={new Date(1970, 1, 1, 6)} defaultDate={new Date(2021, 3, 8)} onSelectEvent={event => alert(event.title)} onSelectSlot={handleSelect} /> */}
+        <Calendar 
+          popup
+          selectable 
+          localizer={localizer} 
+          views={{month: true}} 
+          events={events} 
+          style={{ height: "90vh", width: "150vh" }} 
+          onSelectEvent={event => getScheduleById(event)} 
+          />
       </Page>
       <CreateScheduleModal scheduleModal={scheduleModal} setScheduleModal={setScheduleModal} />
       <EditScheduleModal editModal={editModal} setEditModal={setEditModal} eventObj={eventObj} setEventObj={handleEventObj} />
